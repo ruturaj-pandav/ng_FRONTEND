62a5e98fbe40767b7cbe93838628c9d862b4fa0c
rutu-pandav <ruturajpandav@outlook.com>
2023-02-27 15:10:56 +0530

    ab test works analytics

--- src/components/ABTestAnalyticsFigures.js
+++ src/components/ABTestAnalyticsFigures.js
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import swal from "sweetalert";
 import Modal from "./Modal.js";
+import Loader from "./Loader.js";
 import NavbarLogin from "./Navbar_Login";
 import Switch from "react-switch";
 export default function ABTestAnalyticsFigures({ ab_analytics }) {
@@ -20,8 +21,9 @@ export default function ABTestAnalyticsFigures({ ab_analytics }) {
   let navigate = useNavigate();
   const [loader, setloader] = useState(true);
   const [loggedin, setloggedin] = useState(false);
+  // console.log("ye hai ab" , ab_analytics , ab)
   useEffect(() => {
-    if (ab_analytics.status === true) {
+    if (ab_analytics.hasOwnProperty("ab_notifications")) {
       setloader(false);
     }
   }, [ab_analytics]);
@@ -38,78 +40,92 @@ export default function ABTestAnalyticsFigures({ ab_analytics }) {
 
   return (
     <div className="container mx-auto w-5/6 lg:w-2/3  ">
-      <div className="  md:w-full   block shadow-sm my-3 p-3  mx-2">
-        <span className="block text-gray-500   ">Variant {variant}</span>{" "}
-        <Switch
-          onChange={handleChange_VARIANT}
-          checked={variant === 2 ? true : false}
-        />
-      </div>
-      <div>
-        {" "}
+      analytics here
+      {loader ? (
+        <Loader message="Getting analytiscs... please wait " />
+      ) : ab_analytics.hasOwnProperty("ab_notifications") &&
+        ab_analytics.ab_notifications.length > 0 ? (
         <div>
-          <span className="block  text-gray-400 "> Image</span>
-          <span className="block text-gray-600 text-2xl  ">
-            {ab_analytics.image_type}
-          </span>
+          <div className="  md:w-full   block shadow-sm my-3 p-3  ">
+            <span className="block text-gray-500   ">Variant {variant}</span>{" "}
+            <Switch
+              onChange={handleChange_VARIANT}
+              checked={variant === 2 ? true : false}
+            />
+          </div>
+          <div className="border ">
+            {" "}
+            <div>
+              <span className="block  text-gray-400 "> Image</span>
+              <span className="block text-gray-600 text-2xl  ">
+                {ab_analytics.image_type}
+              </span>
+            </div>
+            <div className="grid grid-cols-2 my-5  ">
+              <div className=" col-span-2 sm:col-span-1 my-3 sm:my-0">
+                <span className="block  text-gray-400 "> Redirect URL</span>
+                <a
+                  href={ab_analytics.ab_notifications[variant - 1].redirect_url}
+                  target="_blank"
+                  className="block text-blue-400 cursor-pointer  text-2xl  "
+                >
+                  {ab_analytics.ab_notifications[variant - 1].redirect_url}
+                </a>
+              </div>
+              <div className=" col-span-2 sm:col-span-1 my-3 sm:my-0">
+                <span className="block  text-gray-400 "> Status</span>
+                <span
+                  className={`block text-gray-600 text-2xl   ${
+                    ab_analytics.ab_notifications[variant - 1].status ===
+                      "Completed" && "text-green-500"
+                  } `}
+                >
+                  {ab_analytics.ab_notifications[variant - 1].status}
+                </span>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 my-5  ">
+              <div className=" col-span-2 sm:col-span-1 my-3 sm:my-0">
+                <span className="block  text-gray-400 "> Targeted</span>
+                <span className="block text-gray-600 text-2xl  ">
+                  {ab_analytics.ab_notifications[variant - 1].targeted}
+                </span>
+              </div>
+              <div className=" col-span-2 sm:col-span-1 my-3 sm:my-0">
+                <span className="block  text-gray-400 "> Delivered</span>
+                <span className="block text-gray-600 text-2xl  ">
+                  {ab_analytics.ab_notifications[variant - 1].delivered}
+                </span>
+              </div>
+            </div>
+            <div className="grid grid-cols-3 my-5 ">
+              <div className=" col-span-3   sm:col-span-1 my-3 sm:my-0">
+                <span className="block  text-gray-400 "> Failed</span>
+                <span className="block text-gray-600 text-2xl  ">
+                  {ab_analytics.ab_notifications[variant - 1].failed}
+                </span>
+              </div>
+              <div className=" col-span-3 sm:col-span-1 my-3 sm:my-0">
+                <span className="block  text-gray-400 "> Clicks</span>
+                <span className="block text-gray-600 text-2xl  ">
+                  {ab_analytics.ab_notifications[variant - 1].clicks}
+                </span>
+              </div>
+              <div className=" col-span-3 sm:col-span-1 my-3 sm:my-0">
+                <span className="block  text-gray-400 ">
+                  {" "}
+                  Click Through Rate
+                </span>
+                <span className="block text-gray-600 text-2xl  ">
+                  {ab_analytics.ab_notifications[variant - 1].clickThroughRate}
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="grid grid-cols-2 my-5  ">
-          <div className=" col-span-2 sm:col-span-1 my-3 sm:my-0">
-            <span className="block  text-gray-400 "> Redirect URL</span>
-            <a
-              href={ab_analytics.redirect_url}
-              target="_blank"
-              className="block text-blue-400 cursor-pointer  text-2xl  "
-            >
-              {ab_analytics.redirect_url}
-            </a>
-          </div>
-          <div className=" col-span-2 sm:col-span-1 my-3 sm:my-0">
-            <span className="block  text-gray-400 "> Status</span>
-            <span
-              className={`block text-gray-600 text-2xl   ${
-                ab_analytics.status === "Completed" && "text-green-500"
-              } `}
-            >
-              {ab_analytics.status}
-            </span>
-          </div>
-        </div>
-        <div className="grid grid-cols-2 my-5  ">
-          <div className=" col-span-2 sm:col-span-1 my-3 sm:my-0">
-            <span className="block  text-gray-400 "> Targeted</span>
-            <span className="block text-gray-600 text-2xl  ">
-              {ab_analytics.targeted}
-            </span>
-          </div>
-          <div className=" col-span-2 sm:col-span-1 my-3 sm:my-0">
-            <span className="block  text-gray-400 "> Delivered</span>
-            <span className="block text-gray-600 text-2xl  ">
-              {ab_analytics.delivered}
-            </span>
-          </div>
-        </div>
-        <div className="grid grid-cols-3 my-5 ">
-          <div className=" col-span-3   sm:col-span-1 my-3 sm:my-0">
-            <span className="block  text-gray-400 "> Failed</span>
-            <span className="block text-gray-600 text-2xl  ">
-              {ab_analytics.failed}
-            </span>
-          </div>
-          <div className=" col-span-3 sm:col-span-1 my-3 sm:my-0">
-            <span className="block  text-gray-400 "> Clicks</span>
-            <span className="block text-gray-600 text-2xl  ">
-              {ab_analytics.clicks}
-            </span>
-          </div>
-          <div className=" col-span-3 sm:col-span-1 my-3 sm:my-0">
-            <span className="block  text-gray-400 "> Click Through Rate</span>
-            <span className="block text-gray-600 text-2xl  ">
-              {ab_analytics.clickThroughRate}
-            </span>
-          </div>
-        </div>
-      </div>
+      ) : (
+        "something"
+      )}
     </div>
   );
 }
